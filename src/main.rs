888600f3a46b273d35c3808aed668e97a05cf344
Kane Rogers <kane.m.rogers@gmail.com>
2022-04-03 01:13:48 +1100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ mod game;
 mod player_physics;
 mod ui;
 use bevy::prelude::*;
-use bevy_rapier2d::prelude::*;
+use bevy_rapier2d::{na::Isometry2, prelude::*};
 use player_physics::{player_movement, Player};
 
-const SCREEN_HEIGHT: f32 = 1024.0;
-const SCREEN_WIDTH: f32 = 1324.0;
+const SCREEN_HEIGHT: f32 = 1000.0;
+const SCREEN_WIDTH: f32 = 1300.0;
+const TILE_SIZE: f32 = 50.;
 
 fn main() {
     App::new()
@@ -26,6 +27,7 @@ fn main() {
         .add_system(player_movement)
         .add_system(ui::update)
         .add_system(game::logic)
+        .add_plugin(RapierRenderPlugin)
         .run();
 }
 
@@ -33,50 +35,85 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut rapier_config: ResMut<RapierConfiguration>,
+    windows: Res<Windows>,
 ) {
+    let window = windows.get_primary().unwrap();
+    println!("Window scale: {:?}", window.scale_factor());
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-    let shower_x = (-SCREEN_WIDTH / 2.) + 64.;
-    let shower_y = (SCREEN_HEIGHT / 2.) - 96.;
-    let translation = [shower_x, shower_y, 0.].into();
-    println!("Shower translation: {:?}", translation);
+    let sprite_size_x = 100.0;
+    let sprite_size_y = 150.0;
 
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("shower.png"),
-        transform: Transform {
-            translation,
-            ..Default::default()
-        },
-        ..Default::default()
-    });
-
-    let sprite_size_x = 128.0;
-    let sprite_size_y = 192.0;
+    println!("Sprite size: x: {:?} y: {:?}", sprite_size_x, sprite_size_y);
 
     // Set the scale
-    rapier_config.scale = 64.0;
+    rapier_config.scale = TILE_SIZE;
 
     // Set gravity
     rapier_config.gravity = [0., 0.].into();
 
     // Set the size of the collider
-    let collider_size_x = sprite_size_x / rapier_config.scale;
-    let collider_size_y = sprite_size_y / rapier_config.scale;
+    let collider_size_x = (sprite_size_x / rapier_config.scale) / 2.;
+    let collider_size_y = (sprite_size_y / rapier_config.scale) / 2.;
 
+    println!(
+        "Collider size: x: {:?} y: {:?}",
+        collider_size_x, collider_size_y
+    );
+
+    // commands
+    //     .spawn_bundle(SpriteBundle {
+    //         texture: asset_server.load("player.png"),
+    //         transform: Transform {
+    //             translation: Vec3::new(0., 0., 1.),
+    //             ..Default::default()
+    //         },
+    //         ..Default::default()
+    //     })
+    //     .insert(Player { speed: 300.0 })
+    //     .insert_bundle(RigidBodyBundle {
+    //         mass_properties: (RigidBodyMassPropsFlags::ROTATION_LOCKED).into(),
+    //         ..Default::default()
+    //     })
+    //     .insert_bundle(ColliderBundle {
+    //         position: [0., 0.].into(),
+    //         shape: ColliderShape::cuboid(collider_size_x, collider_size_y).into(),
+    //         ..Default::default()
+    //     })
+    //     .insert(ColliderPositionSync::Discrete)
+    //     .insert(ColliderDebugRender::with_id(1));
+
+    // commands.spawn_bundle(SpriteBundle {
+    //     texture: asset_server.load("environment.png"),
+    //     transform: Transform {
+    //         translation: [-150., 0., 0.].into(),
+    //         ..Default::default()
+    //     },
+    //     ..Default::default()
+    // });
+
+    let coordinates = (0, 0);
+    let dimensions = (11, 4);
+    add_collider(commands, coordinates, dimensions);
+}
+
+fn add_collider(mut commands: Commands, coordinates: (usize, usize), dimensions: (usize, usize)) {
+    let (x_pos, y_pos) = coordinates;
+    let (width, height) = dimensions;
+    let width = width as f32;
+    let height = height as f32;
+
+    let collider_x = (-SCREEN_WIDTH / 2.) + (x_pos as f32 * TILE_SIZE) + ((width * TILE_SIZE) / 2.);
+    let collider_y =
+        (SCREEN_HEIGHT / 2.) - 30. - (y_pos as f32 * TILE_SIZE) - ((height * TILE_SIZE) / 2.);
+
+    println!("COLLIDER: x pos: {:?}, y pos {:?}", collider_x, collider_y);
     commands
-        .spawn_bundle(SpriteBundle {
-            texture: asset_server.load("player.png"),
-            transform: Transform {
-                translation: Vec3::new(0., 0., 1.),
-                ..Default::default()
-            },
+        .spawn_bundle(ColliderBundle {
+            position: [collider_x / TILE_SIZE, collider_y / TILE_SIZE].into(),
+            shape: ColliderShape::cuboid(width as f32 / 2., height as f32 / 2.).into(),
             ..Default::default()
         })
-        .insert(Player { speed: 300.0 })
-        .insert_bundle(RigidBodyBundle::default())
-        .insert_bundle(ColliderBundle {
-            position: [collider_size_x / 2.0, collider_size_y / 2.0].into(),
-            ..Default::default()
-        })
+        .insert(ColliderDebugRender::with_id(2))
         .insert(ColliderPositionSync::Discrete);
 }
