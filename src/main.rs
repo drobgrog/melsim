1b0e3b10d514ae14de33e5e2f8e39b291f657536
Kane Rogers <kane.m.rogers@gmail.com>
2022-04-02 15:45:29 +1100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
 use bevy::prelude::*;
-use bevy::core::CorePlugin;
-use bevy::input::InputPlugin;
-use bevy::window::WindowPlugin;
-use bevy::winit::WinitPlugin;
-use bevy::render::RenderPlugin;
-use bevy::asset::AssetPlugin;
+
+#[derive(Component)]
+struct Player {}
 
 fn main() {
     App::new()
-        .insert_resource(WindowDescriptor{
+        .insert_resource(WindowDescriptor {
             title: String::from("Melbourne Lockdown Simulator"),
             ..Default::default()
         })
-        .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
-        .add_plugin(CorePlugin::default())
-        .add_plugin(InputPlugin::default())
-        .add_plugin(WindowPlugin::default())
-        .add_plugin(WinitPlugin::default())
-        .add_plugin(AssetPlugin::default())
-        .add_plugin(RenderPlugin::default())
-        //.add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(hello_world_system)
+        .add_system(sprite_movement_system)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+fn sprite_movement_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Player, &mut Transform)>,
+) {
+    let (_, mut transform) = query.get_single_mut().unwrap();
+
+    let mut direction = 0.0;
+    if keyboard_input.pressed(KeyCode::Left) {
+        direction -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        direction += 1.0;
+    }
+
+    let translation = &mut transform.translation;
+    translation.x += direction;
+    translation.x = translation.x.min(380.0).max(-380.0);
 }
 
-fn hello_world_system() {
-    println!("hello, world!");
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    let transform = Transform {
+        translation: Vec3::new(0., 0., 0.),
+        scale: Vec3::new(0.1, 0.1, 0.1),
+        rotation: Default::default(),
+    };
+    let mut e = commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("test.png"),
+        transform,
+        ..Default::default()
+    });
+    e.insert(Player {});
 }
